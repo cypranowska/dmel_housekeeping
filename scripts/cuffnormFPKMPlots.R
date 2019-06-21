c178fc4ba2fb7590ebc58b5f4981d41cea6cf462
Shivali <43662457+shivalibaveja@users.noreply.github.com>
2019-06-21 16:36:46 -0400

    Update cuffnormFPKMPlots.R

--- scripts/cuffnormFPKMPlots.R
+++ scripts/cuffnormFPKMPlots.R
@@ -20,3 +20,16 @@ tab.use[,-1] <- log10(tab.use[,-1]+1)
 
 ## join the fpkm and attr tables
 fpkm_attr <- merge(x = tab.use, y = gene.tab, by = "tracking_id", all.x = TRUE)
+
+## create a histogram for fpkm data
+library(dplyr)
+sample_list <- sample.key %>% pull(sample_name)
+for (sample in sample_list){
+  sample_data <- tab.use[,c("tracking_id",sample)]
+  plot(sample_data)}
+
+##histogram for count?
+library(ggplot2)
+for (sample in sample_list){
+  sample_data <- tab.use[,c("tracking_id",sample)]
+  ggplot(sample_data)}
